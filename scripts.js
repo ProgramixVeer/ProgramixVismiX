a83dfa857ee6826c10558ab5c4c50e9cf7009ebf
ProgramixVeer <92788270+ProgramixVeer@users.noreply.github.com>
2021-10-26 11:46:02 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,5 +2,10 @@ function validate()
 {
 var email=document.getByElementId("email").value;
 var password=document.getByElementId("password").value;
-  if(email==
+  if(email==veer@programixvismi.com password==Aju555);
+  print("Login Successful");
+  return 0;
+}
+else {
+  print("Login Failed")
 }
