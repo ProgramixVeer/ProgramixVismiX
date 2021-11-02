b3a38cf279a94607f16c6ae2afde06d3feacdb43
ProgramixVeer <92788270+ProgramixVeer@users.noreply.github.com>
2021-11-02 10:55:31 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ function validate()
 {
 var email=document.getByElementId("email").value;
 var password=document.getByElementId("password").value;
-  if(email=="veer@programixvismi.com" password=="Admin@123");
+  if(email=='veer@programixvismi.com' password=='Admin@123');
 {  
   window.location = "index.html";
   return false;
 }
-   if(email=="whitex@programixvismi.com" password=="WhiteX465");
+   if(email=='whitex@programixvismi.com' password=='WhiteX465');
 {  
   
   return false;
